a68c38280f6d9ec032f25901f73a4db3ebd15168
Srushti Revoor <srushtirevoor99@gmail.com>
2026-08-20 22:12:39 +0530

    Time: 2 ms (92.00%) | Memory: 47.9 MB (38.76%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int lastNonZero=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[lastNonZero];
+                nums[lastNonZero]=nums[i];
+                nums[i]=temp;
+                lastNonZero++;
+            }
+        }
+        
+    }
+}
